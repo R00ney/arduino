592d66b246263f648c8d00a6b88fd7a978e56dc4
R00ney <ngo1991@gmail.com>
2015-05-22 10:28:09 -0400

    combined roll accel and button scripts

--- nunchuck/libWiiChuck/chuck_demo/chuck_demo.ino
+++ nunchuck/libWiiChuck/chuck_demo/chuck_demo.ino
@@ -9,6 +9,10 @@
 #define MINANGLE -90
 
 
+unsigned long old_time;
+unsigned long new_time;
+
+
 WiiChuck chuck = WiiChuck();
 int angleStart, currentAngle;
 int tillerStart = 0;
@@ -25,22 +29,56 @@ void setup() {
 
 void loop() {
   delay(20);
+  new_time = millis();
   chuck.update(); 
 
-
+  Serial.print("roll ");
   Serial.print(chuck.readRoll());
-    Serial.print(", ");  
+  Serial.print(", ");
+  Serial.print("pitch ");  
   Serial.print(chuck.readPitch());
+  Serial.print(", ");  
+  
+  Serial.print("accX ");
+  Serial.print((int)chuck.readAccelX()); 
+  Serial.print(", ");  
+  Serial.print("accY ");
+  Serial.print((int)chuck.readAccelY()); 
+  Serial.print(", ");  
+  
+  Serial.print("accZ ");
+  Serial.print((int)chuck.readAccelZ()); 
+  Serial.print(", "); 
+
+  Serial.print("joyX ");
+  Serial.print(chuck.readJoyX());
+    Serial.print(", ");
+  Serial.print("accY ");  
+  Serial.print(chuck.readJoyY());
     Serial.print(", ");  
 
-    Serial.print((int)chuck.readAccelX()); 
-    Serial.print(", ");  
-    Serial.print((int)chuck.readAccelY()); 
+  if (chuck.buttonZ) {
+     Serial.print("Z");
+  } else  {
+     Serial.print("-");
+  }
+
     Serial.print(", ");  
 
-    Serial.print((int)chuck.readAccelZ()); 
+//not a function//  if (chuck.buttonC()) {
+  if (chuck.buttonC) {
+     Serial.print("C");
+  } else  {
+    Serial.print("-");
+  }
+
+
+    //print time diff of loop
+   Serial.print(", dT ");
+   old_time = millis();
+   Serial.print( old_time - new_time);
+   
 
     Serial.println();
-
 }
  
